07ffe77e87ca084b9b2011d8b47be6ef8f175b68
Mauricio Otalora <cmotalora@sena.edu.co>
2023-11-10 09:55:51 -0500

    global introduccion custom css portada

--- src/config/global.js
+++ src/config/global.js
@@ -1,9 +1,10 @@
 export default {
   global: {
-    componenteFormativo: 'NOMBRE DEL COMPONENTE FORMATIVO',
-    descripcionCurso: 'BREVE DESCRIPCIÓN',
+    componenteFormativo: 'Componentes ambientales',
+    descripcionCurso:
+      'Se abordarán temas que permitirán tener una concepción clara de los componentes ambientales, cómo determinarlos y evaluarlos, la manera de participar comunitariamente para defenderlos conociendo las leyes que los abarcan, así mismo obtendrá conocimientos sobre las técnicas educativas para transmitir este conocimiento.',
     imagenBannerPrincipal: require('@/assets/curso/portada/banner-princiapal.svg'),
-    fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
+    fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.svg'),
     imagenesDecorativasBanner: [
       {
         clases: ['banner-principal-decorativo-1', 'd-none', 'd-lg-block'],
@@ -31,28 +32,115 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Aspectos ambientales',
         desarrolloContenidos: true,
-        subMenu: [
-          {
-            numero: '1.1',
-            titulo: 'Titulo de segundo nivel',
-            hash: 't_1_1',
-          },
-        ],
       },
 
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Impactos ambientales',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '2.1',
+            titulo: 'Normatividad',
+            hash: 't_2_1',
+          },
+          {
+            numero: '2.2',
+            titulo: 'Valoración de aspectos e impactos ambientales',
+            hash: 't_2_2',
+          },
+          {
+            numero: '2.3',
+            titulo: 'Matriz de Leopold',
+            hash: 't_2_3',
+          },
+        ],
       },
       {
         nombreRuta: 'tema3',
         numero: '3',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Normatividad ambiental',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '3.1',
+            titulo: 'Recursos naturales',
+            hash: 't_3_1',
+          },
+          {
+            numero: '3.2',
+            titulo: 'Conservación ambiental',
+            hash: 't_3_2',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema4',
+        numero: '4',
+        titulo: 'Promotoría ambiental',
+        desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'tema5',
+        numero: '5',
+        titulo: 'Educación ambiental',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '5.1',
+            titulo: ' Capacitación',
+            hash: 't_5_1',
+          },
+          {
+            numero: '5.2',
+            titulo: 'Técnicas educativas',
+            hash: 't_5_2',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema6',
+        numero: '6',
+        titulo: 'Programa ambiental',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '6.1',
+            titulo: 'Norma ISO 14001',
+            hash: 't_6_1',
+          },
+          {
+            numero: '6.2',
+            titulo: 'Plan de acción',
+            hash: 't_6_1',
+          },
+          {
+            numero: '6.3',
+            titulo: 'Gestión documental',
+            hash: 't_6_3',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema7',
+        numero: '7',
+        titulo: 'Participación comunitaria',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '7.1',
+            titulo: 'Niveles',
+            hash: 't_7_1',
+          },
+          {
+            numero: '7.2',
+            titulo: 'Mecanismos constitucionales',
+            hash: 't_7_2',
+          },
+        ],
       },
     ],
     subMenu: [
--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -1,15 +1,29 @@
 <template lang="pug">
-.curso-main-container.introduccion
-  BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-  
-    .titulo-principal.color-acento-contenido
-      .titulo-principal__numero
-        span
-          i.fas.fa-info
-      h1 Introducción
+  .curso-main-container.introduccion
+    BannerInterno
+    .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
     
-</template>
+      .titulo-principal.color-acento-contenido
+        .titulo-principal__numero
+          span
+            i.fas.fa-info
+        h1 Introducción
+  
+  
+      .row.align-items-center.my-3.col-lg-12.m-0        
+        p.mb-2.py-1(style="overflow-x: hidden !important") Los componentes ambientales son elementos fundamentales que constituyen el entorno natural. Comprenden factores bióticos, como la flora y la fauna, así como factores abióticos, como el agua, el aire, el suelo y el clima. Estos componentes interactúan de manera compleja y delicada para mantener el equilibrio ecológico en el planeta. La salud y la calidad de vida de los seres vivos, incluyendo a los seres humanos, dependen en gran medida de la integridad de estos componentes. La conservación y la gestión sostenible de los recursos naturales son cruciales para preservar la biodiversidad y garantizar un ambiente saludable para las generaciones futuras.
+  
+      figure.mt-5
+        .video
+          iframe(width="560" height="315" src="https://www.youtube.com//embed/" title="Video Huella Hidrica" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+  
+      
+  
+  
+  
+  
+      
+  </template>
 
 <script>
 export default {
