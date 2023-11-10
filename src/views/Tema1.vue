cb44ce36acd3e7a4be1e1d700e7c9894fe1ce155
Mauricio Otalora <cmotalora@sena.edu.co>
2023-11-10 15:35:41 -0500

    tema1 terminado

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -16,7 +16,142 @@
 
         p.mb-4(style="overflow-x: hidden !important") El equilibrio en el medio ambiente puede verse afectado con el tiempo, ya sea por causas naturales o por la influencia humana. Aunque todos los elementos del ambiente pueden adaptarse a nuevas condiciones ambientales, los límites de tolerancia pueden verse comprometidos si no se permite el tiempo adecuado para aclimatarse.
 
-    
+    .row.align-items-center.my-5.col-lg-12.mb-2(style="background:#FFEBDF")           
+      .col-1.p-1
+      .col-4.p-1
+          img(src="@/assets/curso/temas/1/2.svg" style="max-width: 65%; display: block; margin-top: -62px; margin-bottom: -32px;  " ) 
+      .col-7.p-1
+        .col-sm.my-3.mb-sm-0.px-1
+          ul.lista-ul--color
+            li 
+              | Es importante tener en cuenta que los seres humanos son un elemento destacado en este sistema y están modificando su comportamiento para adaptarse a las nuevas condiciones. Esta adaptación afecta los límites de tolerancia a medida que transcurre el tiempo y aumenta la diversidad en términos de requerir materias primas derivadas de los recursos naturales, como el agua utilizada en la agricultura o los combustibles para la generación de energía. Debido a esto, el medio ambiente se ve afectado por la explotación de recursos y la necesidad de una mayor cobertura cuando los seres humanos están en proceso de adaptación y expansión.
+
+    p.mb-4(style="overflow-x: hidden !important") Los aspectos ambientales se pueden analizar desde cinco perspectivas principales:
+
+    .row.mb-4
+      .col-auto.my-5.mb-sm-0.mb-5        
+        ol.lista-ol--cuadro(style="border-radius: 20px; background-color: #C1E29A") 
+          li 
+            .lista-ol--cuadro__vineta(style="border-radius: 10px; background-color: #C1E29A") 
+              .col  
+              img(src="@/assets/curso/temas/1/3.svg" style="max-width: 65%; display: block; margin-top: -12px; " ) 
+            b.mx-3 Tipología
+
+    p.mb-4(style="overflow-x: hidden !important") Se refiere a la clasificación o categorización de diferentes elementos o características del medio ambiente y sus interacciones con actividades humanas o procesos industriales. Estas se pueden tipificar de la siguiente manera.
+
+    .row.justify-content-center.my-5(data-aos="fade-right")
+      .col-lg-10 
+        .titulo-sexto.mb-0.color-acento-contenido.mb-4
+          p.mb-0 #[b  Figura 1.] 
+            i  Tipología de los aspectos ambientales
+
+        .row
+          figure.desktop.d-none.d-sm-block.d-sm-none.d-lg-block.d-none.d-md-block
+            img.mb-1(src='@/assets/curso/temas/1/7.svg' alt='En la figura 1 se muestra la manera como se pueden tipificar los aspectos ambientales, según las emisiones atmosféricas, residuos peligrosos, residuos inertes, vertidos de agua, consumo de energía y consumo de agua.').m-auto(data-aos="fade-right")
+          figure.movil.mt-0
+            .row.justify-content-center.d-lg-none.d-md-none        
+              img(src="@/assets/curso/temas/1/8.svg" data-aos="zoom-in-up")
+
+    .row.mb-4
+      .col-auto.my-5.mb-sm-0.mb-5        
+        ol.lista-ol--cuadro(style="border-radius: 20px; background-color: #C1E29A") 
+          li 
+            .lista-ol--cuadro__vineta(style="border-radius: 10px; background-color: #C1E29A") 
+              .col  
+              img(src="@/assets/curso/temas/1/3.svg" style="max-width: 65%; display: block; margin-top: -12px; " ) 
+            b.mx-3 Clasificación  
+
+    p.mb-4(style="overflow-x: hidden !important") Se refiere a la categorización o agrupación de estos aspectos según diferentes criterios o características comunes. Esto facilita su identificación, evaluación y gestión en el contexto de la gestión ambiental de organizaciones o actividades. Según su funcionamiento, se pueden clasificar de la siguiente manera:
+
+    .row.align-items-center.my-3.col-lg-12.mb-2 
+      .col-1         
+      .col-10.p-3(style="background:#E7FFD1; border-radius: 20px;")
+        .col-sm.mb-3.mb-sm-0.px-5
+          ul.lista-ul--color
+            li 
+              i.fas.fa-check
+              | Significativos: se refieren a aquellos aspectos de una actividad, proceso o servicio que tienen un impacto ambiental importante o que pueden influir en la capacidad de una organización para alcanzar sus objetivos ambientales o cumplir con las regulaciones y requisitos ambientales.
+
+    .row.align-items-center.my-3.col-lg-12.mb-2 
+      .col-1         
+      .col-10.p-3(style="background:#E7FFD1;  border-radius: 20px;")
+        .col-sm.mb-3.mb-sm-0.px-5
+          ul.lista-ul--color
+            li 
+              i.fas.fa-check
+              | No significativos: son aquellos que, después de una evaluación adecuada, se determina que tienen un impacto ambiental limitado o menor en comparación con otros aspectos.
+
+    .row.mb-4
+      .col-auto.my-5.mb-sm-0.mb-5        
+        ol.lista-ol--cuadro(style="border-radius: 20px; background-color: #C1E29A") 
+          li 
+            .lista-ol--cuadro__vineta(style="border-radius: 10px; background-color: #C1E29A") 
+              .col  
+              img(src="@/assets/curso/temas/1/3.svg" style="max-width: 65%; display: block; margin-top: -12px; " ) 
+            b.mx-3 Identificación
+
+    p.mb-4(style="overflow-x: hidden !important") Este proceso permite a la organización comprender en qué situaciones sus actividades, productos o servicios pueden tener un impacto en el medio ambiente. Al reconocer estas circunstancias, la organización puede identificar los tipos de impactos ambientales que podrían generarse como resultado de sus operaciones. Esto, a su vez, facilita la evaluación y gestión de estos impactos dentro del sistema de gestión ambiental de la organización. La identificación de aspectos ambientales se realiza a través de un proceso de revisión y evaluación de todas las operaciones y actividades de la organización.
+
+    .row.mb-4
+      .col-auto.my-5.mb-sm-0.mb-5        
+        ol.lista-ol--cuadro(style="border-radius: 20px; background-color: #C1E29A") 
+          li 
+            .lista-ol--cuadro__vineta(style="border-radius: 10px; background-color: #C1E29A") 
+              .col  
+              img(src="@/assets/curso/temas/1/3.svg" style="max-width: 65%; display: block; margin-top: -12px; " ) 
+            b.mx-3 Componentes
+
+    p.mb-4(style="overflow-x: hidden !important") Son todos los elementos, factores o recursos naturales que presentan interacciones entre sí, lo cual permite que se genere la vida en el planeta. Estos componentes tienen una vital influencia en el medio ambiente, por ende, con cada uno de los seres vivos. Los componentes ambientales se pueden dividir en los siguientes factores:
+
+    .tarjeta.tarjeta--azul.p-4.my-5(data-aos="fade-right" style="background-color:#E7FFD1")
+      SlyderA(tipo="b")
+        .row.justify-content-center.align-items-center
+          .col-md-6.mb-4.mb-md-0
+            h2 Factores bióticos <br>
+            p.mb-5 Son todos los organismos vivos, como animales, plantas, microorganismos y otros seres vivos, que influyen en un ecosistema mediante sus interacciones y relaciones directas. Estos factores bióticos pueden tener un impacto tanto positivo como negativo en la forma y el funcionamiento de un ecosistema.
+            h2 Los factores bióticos pueden clasificarse de diversas maneras: <br>
+            P ● Según su organización ecológica.
+            P ● Según su posición en la cadena alimentaria o cadena trófica.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/1/4.png', alt='')
+
+        .row.justify-content-center.align-items-center
+          .col-md-6.mb-4.mb-md-0
+            h2 Factores abióticos
+            p Son componentes no vivos del entorno, como la temperatura y la luz solar, que influyen en los ecosistemas y en la capacidad de los seres vivos para sobrevivir y reproducirse. Estos factores físicos son esenciales para el desarrollo de los ecosistemas y determinan qué tipos de plantas y animales pueden prosperar en un área. Comprender estos factores es crucial para la ecología y la conservación de la biodiversidad.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/1/5.png', alt='')
+
+        .row.justify-content-center.align-items-center
+          .col-md-6.mb-4.mb-md-0
+            h2 Factores socioculturales <br>
+            p.mb-5 Se refieren a un conjunto de influencias y características en la sociedad que pueden tener un impacto en las actitudes, comportamientos y valores de las personas. Estos factores a menudo se dividen en dos categorías principales:
+
+            b ● Factores internos: 
+            | estos incluyen las creencias, valores, actitudes y habilidades personales de un individuo. <br>
+            
+            b ● Factores externos: 
+            | estos se refieren al efecto del entorno sociocultural más amplio en las personas. Esto puede incluir la influencia de la educación, la familia, la religión, los medios de comunicación, las normas culturales y otros aspectos de la sociedad en la que una persona vive.
+
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/1/4.png', alt='')
+
+    .row.mb-4
+      .col-auto.my-5.mb-sm-0.mb-5        
+        ol.lista-ol--cuadro(style="border-radius: 20px; background-color: #C1E29A") 
+          li 
+            .lista-ol--cuadro__vineta(style="border-radius: 10px; background-color: #C1E29A") 
+              .col  
+              img(src="@/assets/curso/temas/1/3.svg" style="max-width: 65%; display: block; margin-top: -12px; " ) 
+            b.mx-3 Desarrollo sostenible
+
+    p.mb-4(style="overflow-x: hidden !important") Se enfoca en garantizar que la sociedad pueda satisfacer sus necesidades básicas sin perjudicar los ecosistemas ni comprometer las necesidades de las generaciones futuras. Este enfoque promueve la responsabilidad ambiental, la preservación de los recursos naturales y el equilibrio entre el crecimiento económico, la protección del medio ambiente y el bienestar social. Busca perpetuar a la especie humana mediante un modelo de desarrollo que permita un presente próspero sin comprometer el futuro de nuestro planeta.
+
+
+
       
 </template>
 
