1a9037f4273dd84087ee9701fe425232e7d18939
Mauricio Otalora <cmotalora@sena.edu.co>
2023-11-11 15:24:27 -0500

    tema4 - imagenes terminado

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -5,7 +5,78 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 3
-      h1 Titulo de primer nivel
+      h1 Promotoría ambiental
+
+    .row.justify-content-center(data-aos="fade-right")
+      .col-lg-12
+        .bloque-texto-g.bloque-texto-g--inverso.color-primario.p-3.p-sm-4.p-md-5.mb-4
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/temas/4/1.svg')})`}"
+          )
+          .bloque-texto-g__texto.p-4
+            p.mb-0 La promotoría ambiental desempeña un papel fundamental en la promoción de la conciencia y el cuidado del medio ambiente, contribuyendo a la construcción de un futuro basado en modelos de desarrollo sostenible. Los promotores ambientales son actores clave en la educación ambiental y lideran proyectos y programas destinados a proteger el medio ambiente y fomentar la sensibilización y la educación en sus comunidades. Su labor se enmarca en la participación ciudadana, lo que incluye supervisar la gestión ambiental y organizar a las comunidades para abordar problemas ambientales. Entre sus funciones se encuentran:
+
+    p.py-3 Se deben establecer las relaciones de causa y efecto entre aspectos y sus impactos, ya que esto ayuda a identificar cuáles son significativos y, por lo tanto, requieren atención y gestión prioritaria. La identificación de aspectos ambientales significativos y sus impactos asociados es fundamental para la toma de decisiones y la planificación de acciones de gestión ambiental.
+
+    .row.justify-content-center.mb-5
+      .col-lg-11.position-relative
+
+        .crd.py-1
+          .row.align-items-center
+            .col-1
+              figure
+                img(src="@/assets/curso/temas/4/2.svg", alt="alt").img150
+            .col.m-1
+              .titulo-principal.color-acento-botones.pt-4  Conocer y difundir todas las políticas referentes a la educación ambiental para dar a conocer al público sus diferentes estrategias y objetivos. 
+        
+        .crd.py-1 
+          .row.align-items-center
+            .col-1
+              figure
+                img(src="@/assets/curso/temas/4/3.svg", alt="alt").img150
+            .col.m-1
+              .titulo-principal.color-acento-botones.pt-4 El desarrollo de los diferentes procesos de gestión para lograr sensibilizar, educar y capacitar a la comunidad. 
+
+        .crd.py-1
+          .row.align-items-center
+            .col-1
+              figure
+                img(src="@/assets/curso/temas/4/4.svg", alt="alt").img150
+            .col.m-1
+              .titulo-principal.color-acento-botones.pt-4 Articular las organizaciones con índoles sociales a los procesos ambientales bien sea locales o regionales. 
+
+        .crd.py-1
+          .row.align-items-center
+            .col-1
+              figure
+                img(src="@/assets/curso/temas/4/5.svg", alt="alt").img150
+            .col.m-1
+              .titulo-principal.color-acento-botones.pt-4  Estará encargado de estimular la participación ciudadana para garantizar que las comunidades se organicen en pro de satisfacer sus necesidades. 
+        
+        .crd.py-1  
+          .row.align-items-center
+            .col-1
+              figure
+                img(src="@/assets/curso/temas/4/6.svg", alt="alt").img150
+            .col.m-1
+              .titulo-principal.color-acento-botones.pt-4  Ser un intermediario de la comunidad y las instituciones.
+
+    p.py-3 En Colombia, el Ministerio de Ambiente y Desarrollo Sostenible, ha implementado el “Programa Promotores Ambientales Comunitarios”, como parte de la Política Nacional de Educación Ambiental "Formación de Educadores y Dinamizadores Ambientales", con el objetivo de fomentar y mejorar la participación ciudadana en la planificación, gestión y seguimiento de políticas, planes, programas y proyectos que promuevan el desarrollo sostenible y la educación ambiental a nivel local y regional. Las funciones de los promotores ambientales comunitarios incluyen:
+
+    PasosA.color-acento-contenido.mb-5(tipo="l")
+      div
+        p Conocer y difundir la Política Nacional de Educación Ambiental y sus estrategias.
+      div
+        p Adelantar los procesos que sean necesarios para una correcta gestión de la sensibilización, educación y capacitación de las comunidades.
+      div
+        p Articular las organizaciones sociales con los procesos ambientales que se realicen a nivel local y regional. 
+      div
+        p Fortalecer la participación ciudadana y la capacidad que tienen para organizarse y tomar decisiones. 
+      div
+        p Ser el intermediario entre la comunidad y las instituciones territoriales.
+      div
+        p Participar de los procesos de planeación de la gestión ambiental. 
+
 
 </template>
 
