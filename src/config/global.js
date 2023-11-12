ff88e8d524009d52302bca1b4c78a3870f0a2714
Mauricio Otalora <cmotalora@sena.edu.co>
2023-11-11 22:18:57 -0500

    tema7 terminado - CF completo

--- src/config/global.js
+++ src/config/global.js
@@ -185,31 +185,121 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: 'https://www.google.com/',
+      tema: '1. Aspectos Ambientales',
+      referencia: 'Lina Bejarano, 2018. Componentes Ambientales. YouTube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=_jl2o3uk5P8',
     },
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      tema: '2. Aspectos Ambientales',
+      referencia:
+        'UNESCO en español, Los Objetivos de Desarrollo Sostenible - qué son y cómo alcanzarlos, YouTube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=MCKH5xk8X-g',
+    },
+    {
+      tema: '3. Normatividad Ambiental',
+      referencia:
+        'Esteban Correa, Introducción a la Normatividad Ambiental en Colombia (Sonido Mejorado), YouTube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=XPeC4Fw_T10',
     },
   ],
   glosario: [
     {
-      termino: '',
-      significado: '',
+      termino: 'Aspecto ambiental:',
+      significado:
+        'actividades humanas que tienen influencia sobre el entorno en donde se desarrollan.',
     },
     {
-      termino: '<em></em>',
-      significado: '',
+      termino: 'Componentes ambientales:',
+      significado:
+        'elementos, factores o recursos naturales que presentan interacciones entre sí, lo cual permite que se genere la vida en nuestro planeta.',
+    },
+    {
+      termino: 'Impacto ambiental:',
+      significado:
+        'consecuencias de las actividades humanas que repercuten sobre el medio ambiente generando un impacto ambiental en el entorno en el cual operan ya sea positivo o negativo, el cual incluye el aire, el agua, el suelo, la flora, la fauna, los seres humanos y sus interrelaciones.',
+    },
+    {
+      termino: 'Leyes ambientales:',
+      significado:
+        'corresponden a los principios y normas que buscan la protección, conservación, recuperación del medio ambiente y preservación de los recursos naturales.',
+    },
+    {
+      termino: 'Matriz aspectos e impactos:',
+      significado:
+        'base fundamental para la toma de decisiones frente a los impactos ambientales que se determinen y evalúen dentro de la organización.',
+    },
+    {
+      termino: 'Resolución ambiental:',
+      significado:
+        'lineamientos que la autoridad requiere para la elaboración y ejecución de los estudios ambientales que deben ser presentados ante las autoridades.',
     },
   ],
   referencias: [
     {
-      referencia: '',
+      referencia:
+        'Alcaldía de Ibagué. (s.f.). (2022). Lección 1: Concepto de participación comunitaria. En Participación comunitaria.',
+      link:
+        'https://ibague.gov.co/portal/admin/archivos/formacionVirtual/participacioncomunitaria/contenido/pdf/Unidad1Leccion1.pd',
+    },
+    {
+      referencia:
+        'Alfonso Ávila, N. Z. H. (2014). Principales normas ambientales colombianas. Universidad EAN.',
+      link: 'https://files.core.ac.uk/pdf/1162/74477868.pdf',
+    },
+    {
+      referencia:
+        'Carrillo Roa, J. D. y Cacua Peñaloza, S. C. (2019). Educación ambiental en Colombia: hacia un óptimo desarrollo sostenible. Dialéctica, 15(1)',
+      link: 'http://portal.amelica.org/ameli/jatsRepo/88/88741012/html/',
+    },
+    {
+      referencia:
+        'Comisión Económica para América Latina y el Caribe. (s.f.). (2022). Acerca de desarrollo sostenible. Naciones Unidas.',
+      link:
+        'https://www.cepal.org/es/temas/desarrollo-sostenible/acerca-desarrollo-sostenible',
+    },
+    {
+      referencia:
+        'Decreto 1791 de 1996. Por medio del cual se establece el régimen de aprovechamiento forestal.4 de octubre de 1996. D.O. No. 42894.',
+      link: '',
+    },
+    {
+      referencia:
+        'Decreto 2820 de 2010. Por el cual se reglamenta el Título VIII de la Ley 99 de 1993 sobre licencias ambientales. 5 de agosto de 2010. D.O. No. 47792.',
+      link: '',
+    },
+    {
+      referencia:
+        'Delgado Álvarez, C. y Palacios Peña, P. (s.f.).(2022). Técnicas educativas. Universidad del Azuay.',
+      link:
+        'https://www.uazuay.edu.ec/sites/default/files/public/TECNICAS-EDUCATIVAS.pdf',
+    },
+    {
+      referencia:
+        'Ley 134 de 1994. Por la cual se dictan normas sobre mecanismos de participación ciudadana. 31 de mayo de 1994. D.O. No. 41373.',
+      link: '',
+    },
+    {
+      referencia:
+        'Ley 99 de 1993. Por la cual se crea el Ministerio del Medio Ambiente, se reordena el Sector Público encargado de la gestión y conservación del medio ambiente y los recursos naturales renovables, se organiza el Sistema Nacional Ambiental, SINA, y se dictan otras disposiciones. 22 de diciembre de 1993. D.O. No. 41146.',
+      link: '',
+    },
+    {
+      referencia:
+        'Ministerio de Ambiente y Desarrollo Sostenible. (s.f.). (2022). Programa Nacional de Promotoría Ambiental Comunitaria. Minambiente.',
+      link:
+        'https://archivo.minambiente.gov.co/index.php/ordenamiento-ambiental-territorial-y-coordinacion-del-sina/educacion-y-participacion/programa-nacional-de-promotoria-ambiental-comunitaria',
+    },
+    {
+      referencia:
+        'Resolución 1023 de 2005. Por la cual se adoptan guías ambientales como instrumento de autogestión y autorregulación. 4 de agosto de 2005. D.O. No. 45990.',
+      link: '',
+    },
+    {
+      referencia:
+        'Resolución 1447 de 2018. Por la cual se reglamenta el sistema de monitoreo, reporte y verificación de las acciones de mitigación a nivel nacional de que trata el artículo 175 de la Ley 1753 de 2015, y se dictan otras disposiciones. 2 de agosto de 2018. D.O. No. 50673.',
       link: '',
     },
   ],
@@ -223,9 +313,9 @@ export default {
           centro: 'Dirección General',
         },
         {
-          nombre: 'Nombre completo',
+          nombre: 'Norma Constanza Morales Cruz',
           cargo: 'Responsable de línea de producción',
-          centro: 'Nombre centro de formación',
+          centro: '"Regional Tolima - Centro de Comercio y Servicios"',
         },
       ],
     },
@@ -233,9 +323,49 @@ export default {
       titulo: 'CONTENIDO INSTRUCCIONAL',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Nombre del rol',
-          centro: 'Nombre centro de formación',
+          nombre: 'Víctor Julián Ardila ',
+          cargo: 'Experto Temático ',
+          centro: '"Regional Tolima - Centro Agropecuario la Granja. "',
+        },
+        {
+          nombre: 'Gustavo Santis Mancipe ',
+          cargo: 'Diseñador Instruccional ',
+          centro:
+            '"Regional Distrito Capital - Centro de Gestión Industrial. "',
+        },
+        {
+          nombre: 'Rafael Neftalí Lizcano Reyes ',
+          cargo: 'Responsable Equipo Desarrollo Curricular ',
+          centro:
+            '"Regional Santander - Centro Industrial del Diseño y la Manufactura. "',
+        },
+        {
+          nombre: 'Carolina Coca Salazar ',
+          cargo: 'Asesora Metodológica ',
+          centro:
+            '"Regional Distrito Capital - Centro de Diseño y Metrología. "',
+        },
+        {
+          nombre: 'Sandra Patricia Hoyos Sepúlveda ',
+          cargo: 'Corrección de Estilo ',
+          centro:
+            '"Regional Distrito Capital - Centro de Diseño y Metrología. "',
+        },
+        {
+          nombre: 'Jaslyth Juliana Eraso Casanova ',
+          cargo: 'Experta Temática ',
+          centro:
+            '"Regional Putumayo - Centro Agroforestal y Acuícola Arapaima. "',
+        },
+        {
+          nombre: 'Sergio Augusto Ardila Ortiz',
+          cargo: 'Diseñador instruccional ',
+          centro: '"Regional Tolima - Centro de Comercio y Servicios"',
+        },
+        {
+          nombre: 'Viviana Esperanza Herrera Quiñonez',
+          cargo: 'Metodóloga',
+          centro: '"Regional Tolima - Centro de Comercio y Servicios"',
         },
       ],
     },
@@ -243,19 +373,39 @@ export default {
       titulo: 'DISEÑO Y DESARROLLO DE RECURSOS EDUCATIVOS DIGITALES',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Diseñador web',
-          centro: 'Nombre centro de formación',
+          nombre: 'José Yobani Penagos Mora',
+          cargo: 'Diseñador de Contenidos Digitales',
+          centro: '"Regional Tolima - Centro de Comercio y Servicios"',
         },
         {
-          nombre: 'Nombre responsable',
+          nombre: 'Cristian Mauricio Otálora Clavijo',
           cargo: 'Desarrollador Fullstack',
-          centro: 'Nombre centro de formación',
+          centro: '"Regional Tolima - Centro de Comercio y Servicios"',
         },
         {
-          nombre: 'Nombre',
-          cargo: 'Animador y Producción audiovisual',
-          centro: 'Nombre centro de formación',
+          nombre: 'Gilberto Junior Rodríguez Rodríguez',
+          cargo: 'Storyboard de Ilustración',
+          centro: '"Regional Tolima - Centro de Comercio y Servicios"',
+        },
+        {
+          nombre: 'Maria Alejandra Briceño Vera',
+          cargo: 'Producción',
+          centro: '"Regional Tolima - Centro de Comercio y Servicios"',
+        },
+        {
+          nombre: 'Nelson Iván Vera Briceño',
+          cargo: 'Producción audiovisual',
+          centro: '"Regional Tolima - Centro de Comercio y Servicios"',
+        },
+        {
+          nombre: 'Oleg Litvin',
+          cargo: 'Animador ',
+          centro: '"Regional Tolima - Centro de Comercio y Servicios"',
+        },
+        {
+          nombre: 'Cristian Mauricio Otálora Clavijo',
+          cargo: 'Actividad Didáctica',
+          centro: '"Regional Tolima - Centro de Comercio y Servicios"',
         },
       ],
     },
@@ -263,14 +413,14 @@ export default {
       titulo: 'VALIDACIÓN RECURSO EDUCATIVO DIGITAL',
       autores: [
         {
-          nombre: 'Nombre validador',
-          cargo: 'Validación y vinculación en plataforma LMS',
-          centro: 'Nombre centro formación',
+          nombre: 'Gilberto Naranjo Farfán',
+          cargo: 'Validación de contenidos accesibles',
+          centro: '"Regional Tolima - Centro de Comercio y Servicios"',
         },
         {
-          nombre: 'Nombre validador',
-          cargo: 'Validación de contenidos accesibles',
-          centro: 'Nombre centro formación',
+          nombre: 'Javier Mauricio Oviedo',
+          cargo: 'Validación y vinculación en plataforma LMS',
+          centro: '"Regional Tolima - Centro de Comercio y Servicios"',
         },
       ],
     },
--- src/views/Tema7.vue
+++ src/views/Tema7.vue
@@ -5,7 +5,239 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 7
-      h1 Titulo de primer nivel
+      h1 Participación comunitaria
+
+    p.py-2 La participación comunitaria es el proceso mediante el cual un grupo de individuos toma conciencia de asuntos que afectan sus necesidades y recursos compartidos. Este proceso implica la formulación, ejecución y evaluación de proyectos o actividades que aborden intereses comunes. Además, se relaciona con la capacidad de los miembros de una comunidad para tomar decisiones y actuar colectivamente en función de sus objetivos.
+    p.py-2 La participación comunitaria se mide por el grado de influencia y poder que tienen los individuos en una comunidad para tomar decisiones y llevar a cabo acciones. Representa un enfoque democrático que destaca la importancia de la acción colectiva en la sociedad.
+
+    .row.align-items-center.my-3.col-lg-12.my-4                 
+      .col-7.p-4(style="background:#D5FFF5")
+        p.mb-2(style="overflow-x: hidden !important") El proceso de participación comienza a nivel individual, donde las personas reconocen su potencial para involucrarse activamente en un grupo y contribuir a la toma de decisiones que pueden llevar al cambio. La participación se desarrolla en etapas, y a menudo implica acciones colectivas en espacios públicos accesibles, con el objetivo de abordar las necesidades de la población.
+        p.mb-2(style="overflow-x: hidden !important") La participación comunitaria puede tomar diversas formas, incluyendo aspectos políticos, ciudadanos, sociales y comunitarios. Para ser efectiva, debe ser integral y coherente con los objetivos acordados por la comunidad. Este proceso desempeña un papel fundamental en la promoción del cambio y el empoderamiento de las comunidades para abordar sus desafíos y metas.
+
+      .col-5
+        figure
+          img(src='@/assets/curso/temas/7/1.png').m-auto(data-aos="fade-right")  
+
+    Separador.mt-5  
+
+    .cont_7_1(style="position:relative;")
+      .titulo-segundo.color-acento-contenido(style="position:relative")
+        h2#t_7_1 7.1 Niveles
+
+
+    p.py-5 La participación comunitaria se puede clasificar en dos grupos principales, según los procesos sociales involucrados: de la participación:
+
+    .tarjeta.tarjeta--azul.p-4.my-5(data-aos="fade-right" style="background-color:#E7FFD1")
+      SlyderA(tipo="b")
+        .row.justify-content-center.align-items-center
+          .col-md-6.mb-4.mb-md-0
+            h2 Participación política <br>
+            p.mb-5 Es un proceso que se desarrolla a través de grupos legalmente organizados, como partidos o movimientos políticos, en los cuales los ciudadanos participan en la toma de decisiones y actividades relacionadas con la ejecución de acciones públicas.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/7/2.png', alt='')
+
+        .row.justify-content-center.align-items-center
+          .col-md-6.mb-4.mb-md-0
+            h2 Participación ciudadana
+            p Implica que los ciudadanos puedan involucrarse en asuntos de interés político y social que sean de naturaleza colectiva. Es la manera en que los ciudadanos respaldan la democracia al participar en las decisiones y la gestión de asuntos públicos.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/7/3.png', alt='')
+
+        .row.justify-content-center.align-items-center
+          .col-md-6.mb-4.mb-md-0
+            h2 Participación comunitaria<br>
+            p.mb-5 Implica que tanto ciudadanos como no ciudadanos se involucren en acciones llevadas a cabo de manera colectiva para abordar las necesidades de su vida diaria. En este contexto, las personas de una comunidad trabajan juntas para encontrar soluciones a los desafíos que enfrentan en su entorno.
+
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/7/4.png', alt='')
+
+        .row.justify-content-center.align-items-center
+          .col-md-6.mb-4.mb-md-0
+            h2 Participación social <br>
+            p.mb-5 Se refiere a la colaboración de ciudadanos que se unen y se organizan en grupos colectivos que no son parte del gobierno o las instituciones públicas. Estos grupos pueden ser de diversa naturaleza, como organizaciones no gubernamentales, asociaciones civiles, o cualquier entidad que no forme parte de las ramas del poder público del Estado. Estos ciudadanos se unen con un propósito común para abordar cuestiones sociales, políticas o comunitarias que les preocupan.
+
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/7/5.png', alt='')
+
+    p.py-5 La participación se puede llevar a cabo a través de diversos métodos y estrategias, dependiendo de los objetivos y las necesidades específicas de la comunidad. Algunos de los métodos más comunes de participación comunitaria incluyen:
+    
+    .row.my-4 
+      .col-1
+      .col-10.p-1(style="background-color: #E7FFD1;")
+        .titulo-sexto.color-acento-contenido.mb-0
+          h5 Reuniones y asambleas comunitarias:
+          span organizar reuniones regulares o asambleas donde los miembros de la comunidad pueden discutir temas importantes, tomar decisiones y expresar sus preocupaciones.
+
+    .row.my-4
+      .col-1
+      .col-10.p-1(style="background-color: #E7FFD1;")
+        .titulo-sexto.color-acento-contenido.mb-0
+          h5 Grupos de trabajo:
+          span establecer grupos de trabajo o comités que se centren en áreas específicas, como la educación, la salud, el medio ambiente, etc. Estos grupos trabajan en proyectos o soluciones relacionados con su área de enfoque.
+
+    .row.my-4 
+      .col-1
+      .col-10.p-1(style="background-color: #E7FFD1;")
+        .titulo-sexto.color-acento-contenido.mb-0
+          h5 Encuestas y cuestionarios:
+          span  realizar encuestas y cuestionarios para recopilar información y opiniones de los miembros de la comunidad. Esto puede ayudar a identificar problemas y prioridades.
+
+    .row.my-4
+      .col-1
+      .col-10.p-1(style="background-color: #E7FFD1;")
+        .titulo-sexto.color-acento-contenido.mb-0
+          h5 Talleres y capacitación:
+          span  ofrecer talleres y capacitación en diversas áreas, como habilidades para la resolución de problemas, gestión de proyectos o educación cívica.
+
+    .row.my-4 
+      .col-1
+      .col-10.p-1(style="background-color: #E7FFD1;")
+        .titulo-sexto.color-acento-contenido.mb-0
+          h5 Proyectos colaborativos:
+          span  trabajar en proyectos colaborativos que aborden desafíos específicos de la comunidad, como la construcción de infraestructuras locales o la promoción de la sostenibilidad ambiental.
+
+    .row.my-4
+      .col-1
+      .col-10.p-1(style="background-color: #E7FFD1;")
+        .titulo-sexto.color-acento-contenido.mb-0
+          h5 Grupos en línea y redes sociales:
+          span utilizar plataformas en línea y redes sociales para promover la participación y la comunicación entre los miembros de la comunidad.
+
+    .row.my-4 
+      .col-1
+      .col-10.p-1(style="background-color: #E7FFD1;")
+        .titulo-sexto.color-acento-contenido.mb-0
+          h5 Consultas públicas: 
+          span realizar consultas públicas sobre decisiones o políticas que afecten a la comunidad. Esto permite que los ciudadanos expresen sus opiniones y preocupaciones.
+
+    .row.my-4
+      .col-1
+      .col-10.p-1(style="background-color: #E7FFD1;")
+        .titulo-sexto.color-acento-contenido.mb-0
+          h5 Programas de educación cívica: 
+          span  ofrecer programas de educación cívica para empoderar a los miembros de la comunidad y brindarles las herramientas necesarias para participar activamente en la toma de decisiones.
+
+    .row.my-4 
+      .col-1
+      .col-10.p-1(style="background-color: #E7FFD1;")
+        .titulo-sexto.color-acento-contenido.mb-0
+          h5 Mesas de diálogo: 
+          span establecer mesas de diálogo donde representantes de la comunidad, líderes locales y funcionarios gubernamentales puedan discutir y resolver problemas de manera conjunta.
+
+    .row.my-4
+      .col-1
+      .col-10.p-1(style="background-color: #E7FFD1;")
+        .titulo-sexto.color-acento-contenido.mb-0
+          h5 Medios de comunicación: 
+          span utilizar medios de comunicación locales, como periódicos comunitarios, estaciones de radio o blogs, para promover la participación y la difusión de información relevante.
+
+    
+    p.py-3 Estos son solo algunos de los métodos de participación comunitaria que se pueden utilizar. La elección de un método específico dependerá de las necesidades y la cultura de la comunidad, así como de los objetivos que se quieran lograr.
+
+    Separador.mt-5  
+
+    .cont_7_2(style="position:relative;")
+      .titulo-segundo.color-acento-contenido(style="position:relative")
+        h2#t_7_2 7.2 Mecanismos constitucionales   
+
+    p.py-3 El estado colombiano se compromete a fomentar y apoyar la creación y fortalecimiento de asociaciones profesionales, cívicas, sindicales, comunitarias, juveniles, benéficas y otras organizaciones no gubernamentales. El propósito de estas organizaciones es establecer mecanismos democráticos de representación en todas las instancias de participación, concertación, control y supervisión de la gestión pública.
+
+    .row.align-items-center.my-3.col-lg-12.mb-4 
+      .col-5
+        figure
+          img(src='@/assets/curso/temas/7/6.png').m-auto(data-aos="fade-right")            
+      .col-7.pt-4
+        p.mb-4(style="overflow-x: hidden !important") Estos mecanismos permiten que los ciudadanos y grupos de la sociedad civil participen activamente en la toma de decisiones y en la supervisión de las acciones del gobierno. De esta manera, se promueve una democracia participativa y se garantiza que los intereses y preocupaciones de diversos sectores de la sociedad sean tenidos en cuenta en la formulación de políticas y en la gestión pública.
+
+        p.mb-4(style="overflow-x: hidden !important") La promoción y capacitación de estas organizaciones es fundamental para fortalecer la democracia y asegurar que la ciudadanía tenga las herramientas necesarias para ejercer su derecho a participar en la vida política y pública del país. Estos mecanismos contribuyen a una gobernanza más inclusiva y transparente en Colombia. Algunos de los mecanismos más relevantes incluyen:
+
+    .row.align-items-center.my-3.col-lg-12.m-0(style="border-radius: 30px;")     
+    .row.justify-content-center.my-5
+      .col-lg-8.my-5
+        LineaTiempoD.color-primario
+          p(numero="1" titulo="Plebiscito")  
+            ul.lista-ul--color
+              li.mb-4 
+                i.fas.fa-check
+                | Es el pronunciamiento del pueblo una vez es convocado por el presidente de la República, mediante el cual la comunidad apoyará o rechazará una determinada decisión del ejecutivo. 
+
+              li.mb-4 
+                i.fas.fa-check
+                | El presidente de la república, con la firma de todos los ministros, podrá convocar al pueblo para que se pronuncie sobre las políticas del ejecutivo que no requieran aprobación del congreso; excepto las relacionadas con los estados de excepción y el ejercicio de los poderes correspondientes.
+
+              li.mb-4 
+                | La cámara de representantes y el senado de la república estudiarán las razones que tiene el presidente para solicitar un plebiscito, para lo cual cuentan con un periodo de un mes para aprobar o rechazar esta iniciativa.
+
+          p(numero="2" titulo="Iniciativa popular legislativa") 
+            ul.lista-ul--color
+              li.mb-4 
+                i.fas.fa-check
+                | Es el derecho político de un grupo de ciudadanos de presentar proyecto de acto legislativo y de ley ante el congreso de la república, para que sean debatidos y posteriormente aprobados, modificados o negados por la corporación pública correspondiente.
+
+              li.mb-4 
+                i.fas.fa-check
+                | Inicialmente se debe inscribir un comité de promotores, el cual esté respaldado por apoyos representados en firmas que equivalga al 5 por mil del censo electoral. 
+
+              li.mb-4 
+                i.fas.fa-check
+                | Posteriormente se debe inscribir la iniciativa junto con las respectivas firmas, las cuales serán revisadas por la registraduría y en el momento que alcance el umbral de participación, pasará a ser estudiado por la corporación que sea competente de acuerdo con el tema, la cual definirá si este es aprobado o no. 
+
+          p(numero="3" titulo="Referendo") 
+            ul.lista-ul--color
+              li.mb-4 
+                i.fas.fa-check
+                | Es la convocatoria que se le hace a la comunidad para que sea aprobado o rechazado un proyecto de norma jurídica ya vigente. 
+
+              li.mb-4 
+                i.fas.fa-check
+                | Existen dos tipos de referendos: 
+
+              li.mb-4 
+              b ● Aprobatorio: 
+              | sometido a consideración de la comunidad para que decida si lo aprueba o lo rechaza total o parcialmente.  <br>
+              b ● Derogatorio:
+              | sometimiento de una norma que ya ha sido aprobada por el congreso, el concejo municipal o la asamblea departamental a consideración de la comunidad para que tome la decisión de derogar. 
+
+          p(numero="4" titulo="Revocatoria del mandato")  Se refiere al puntaje obtenido después de calificar los riesgos de cada uno de los productos de un establecimiento, según su severidad, ocurrencia de falla (ocurrencia) y afectación (población expuesta).
+            ul.lista-ul--color
+              li.mb-4 
+                i.fas.fa-check
+                | Derecho político que tienen todos los colombianos, por medio del cual dan por terminado el mandato conferido a un gobernador o alcalde.
+
+              li.mb-4 
+                i.fas.fa-check
+                | Si como resultado de la votación no se revoca el mandato del gobernador o alcalde, no podrá volver a intentarse, una revocatoria del mandato, en lo que resta de su período. De lo contrario, de acuerdo con la Ley 134 de 1994 
+                i “el Registrador Nacional del Estado Civil la comunicará al presidente de la República o al gobernador respectivo para que procedan, según el caso, a la remoción del cargo”.
+
+              li.mb-4 
+                i.fas.fa-check
+                | Debe ser aprobada por más del 51 % de los votos de la ciudadanía que participe en dicha convocatoria, siempre y cuando el número de sufragios no sea inferior al 55 % de la votación válida registrada el día en el que se eligió al respectivo mandatario.
+
+          p(numero="5" titulo="Cabildo abierto")  Son los ponderados o pesos de cada componente, su suma debe ser igual a uno (Aroca y Guzmán, 2017).
+            ul.lista-ul--color
+              li.mb-4 
+                i.fas.fa-check
+                | Es la reunión pública de todos los consejos distritales, municipales o de las juntas de administraciones locales, donde la comunidad puede participar directamente con la finalidad de discutir diversos asuntos los cuales sean de interés para los habitantes. 
+
+              li.mb-4 
+                i.fas.fa-check
+                | Cualquier asunto que sea de interés de la comunidad podrá ser material del cabildo abierto, con excepción de proyectos de ordenanza, acuerdos o algún otro acto administrativo. 
+
+              li.mb-4 
+                i.fas.fa-check
+                | Pueden asistir todas las personas que tengan interés en el asunto.
+
+      .col-8.col-lg-4
+        figure
+          img(src='@/assets/curso/temas/7/7.png').m-auto(data-aos="fade-right") 
+     
+        
+        
 
 </template>
 
