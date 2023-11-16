0966617912b711636963908af011dd6553453007
Mauricio Otalora <cmotalora@sena.edu.co>
2023-11-15 21:18:50 -0500

    Ajustes 4 terminado

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -15,7 +15,7 @@
   
       figure.mt-5
         .video
-          iframe(width="560" height="315" src="https://www.youtube.com//embed/" title="Video Huella Hidrica" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          iframe(width="560" height="315" src="https://www.youtube.com//embed/fVcNyFnnm_8" title="Video Huella Hidrica" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
   
       
   
