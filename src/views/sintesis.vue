77dada1d9d4679a2d10cce3bde8d32f7ad2725b9
Sebastian Trujillo <129899570+strujilloa@users.noreply.github.com>
2023-11-29 11:47:10 -0500

    Update hallazgos y empaquetados

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -3,11 +3,6 @@
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
 
-    .titulo-sexto.color-adicional-2.mb-5
-      h2 Saneamiento y salud ambiental
-      i Síntesis: Componentes ambientales 
-
-
     p.mb-5 A continuación, se describe el tema principal del componente formativo Componentes ambientales, que se consideran elementos vitales que configuran nuestro entorno natural. Incluyen la atmósfera con su mezcla de gases, la hidrosfera con sus océanos y cuerpos de agua, la geosfera con su diversidad de suelos y rocas, y la biosfera, hogar de una asombrosa variedad de formas de vida. Estos componentes interactúan en un delicado equilibrio, influyendo en el clima, la biodiversidad y la sustentabilidad. La preservación y protección de estos componentes son cruciales para asegurar un planeta saludable para las generaciones futuras. La comprensión de sus interacciones es esencial para abordar los desafíos ambientales actuales y promover un futuro sostenible.
 
     .row.justify-content-center
